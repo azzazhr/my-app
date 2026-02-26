e8a79dbb26436368b817e1a8f36fa7fe2599483b
azzazhr <azzahraqina70@gmail.com>
2026-02-26 21:03:02 +0700

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1> <br />
+            <p><b>Nama :</b> Azzahra Attaqina</p>
+            <p><b>NIM :</b> 2341720224</p>
+            <p><b>Program Studi :</b> D4 Teknik Informatika</p>
+        </div>
+    );
+}
