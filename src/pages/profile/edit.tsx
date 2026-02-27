cfc7fa62901b954054dc5f620872892dd59be8fa
azzazhr <azzahraqina70@gmail.com>
2026-02-27 13:47:45 +0700

    add Tugas 1 Jobsheet 3

---
+++ src/pages/profile/edit.tsx
@@ -0,0 +1,10 @@
+const EditProfilePage = () => {
+  return (
+    <div>
+      <h1>Edit Profile</h1>
+      <p>Ini halaman edit profile</p>
+    </div>
+  );
+};
+
+export default EditProfilePage;
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Halaman Profile</h1>
+      <p>Ini halaman profile</p>
+    </div>
+  );
+};
+
+export default Profile;
