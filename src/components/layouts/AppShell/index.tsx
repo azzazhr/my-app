c6ec4ebb57aaf087777df7ba1ccddb6d5526fe1a
azzazhr <azzahraqina70@gmail.com>
2026-03-06 14:32:53 +0700

    add Jobsheet 5 (4 - Tanpa Navbar di Login)

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,17 +1,20 @@
+import { useRouter } from "next/router";
 import Navbar from "../navbar";
 import Footer from "../footer";
 
+const disableNavbar = ["/auth/login", "/auth/register"];
 type AppShellProps = {
   children: React.ReactNode;
 }
 
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
+  const { pathname } = useRouter();
   return (
     <div className="app-container">
-      <Navbar />
 
-      <main className="main-content">
+      <main>
+        {!disableNavbar.includes(pathname) && <Navbar /> }
         {children}
       </main>
 
