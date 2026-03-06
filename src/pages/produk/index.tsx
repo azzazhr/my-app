82c08d268027dc0214388c68343bb916be7b2a26
azzazhr <azzahraqina70@gmail.com>
2026-03-06 09:57:18 +0700

    add Jobsheet 4 (Langkah 7 - Navigasi Imperatif router.push)

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,9 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 const produk = () => {
-    return (
-    <div>
-        <h1>Produk User Page</h1>
-    </div>
-    );
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, []);
+
+  return (
+        <div>
+            <h1>Produk User Page</h1>
+        </div>
+  );
 };
 
 export default produk;
