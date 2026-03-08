7f994172ee9a7a691b2435ec716f6d63c0a275c7
azzazhr <azzahraqina70@gmail.com>
2026-03-08 22:19:04 +0700

    add Tugas 1 Jobsheet 6 (menambahkan judul halaman, desc singkat, gambar)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,13 @@
 import styles from '@/styles/404.module.scss';
+import Head from 'next/head';
+
 const Custom404 = () => {
   return (
     <div className={styles.error}>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+      </Head>
+      
       <img src="/page-not-found.png" alt="404" className={styles.error__image} />
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
