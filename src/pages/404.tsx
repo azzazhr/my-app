b1b31169bfeffc7164a536d0f0d74e0d389173a6
azzazhr <azzahraqina70@gmail.com>
2026-03-08 22:47:17 +0700

    add Tugas 3 Jobsheet 6 (membuat tombol kembali ke home menggunakan link navigasi next.js)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,24 @@
-import styles from '@/styles/404.module.scss';
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from '@/styles/404.module.scss';
 
 const Custom404 = () => {
   return (
+    <>
     <div className={styles.error}>
       <Head>
         <title>404 - Halaman Tidak Ditemukan</title>
       </Head>
-      
+
       <img src="/page-not-found.png" alt="404" className={styles.error__image} />
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
-    </div>
+
+      <Link href="/">
+        Kembali ke Home
+      </Link>
+      </div>
+    </>
   );
 };
 
