de0219a82eea6322106f1a6b34c26a23d1f39c15
azzazhr <azzahraqina70@gmail.com>
2026-03-08 14:06:12 +0700

    add Jobsheet 6 (Langkah 2 - Membuat Custom Document)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ export default function Home() {
   return (
     <>
       <div>
+        <head>
+          <title>Praktikum Next.js Pages Router</title>
+        </head>
         <h1>Praktikum Next.js Pages Router</h1> <br />
         <p>Mahasiswa D4 Pengembangan Web</p> <br /><br />
 
