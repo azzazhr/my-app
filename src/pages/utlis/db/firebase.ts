af2253be77d2a99135d7c4502a525956ffb0cc4f
azzazhr <azzahraqina70@gmail.com>
2026-04-03 12:34:59 +0700

    add Jobsheet 7 (Langkah 6 - Install Firebase bagian copas kotak merah ke file firebase.ts)

---
+++ src/pages/utlis/db/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-N_IUmc2grou3TSJN2JdteLvIv-vAXCI",
+  authDomain: "framework-next-21144.firebaseapp.com",
+  projectId: "framework-next-21144",
+  storageBucket: "framework-next-21144.firebasestorage.app",
+  messagingSenderId: "1089940570781",
+  appId: "1:1089940570781:web:acb62a1b8fc8078611ca74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
