88177bcee5bf8a3de1bcdb503b1a12bd1bb1726f
azzazhr <azzahraqina70@gmail.com>
2026-04-03 12:47:08 +0700

    add Jobsheet 7 (Langkah 8 - Konfigurasi Firebase) modifikasi file firebase.ts

--- src/pages/utlis/db/firebase.ts
+++ src/pages/utlis/db/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-N_IUmc2grou3TSJN2JdteLvIv-vAXCI",
-  authDomain: "framework-next-21144.firebaseapp.com",
-  projectId: "framework-next-21144",
-  storageBucket: "framework-next-21144.firebasestorage.app",
-  messagingSenderId: "1089940570781",
-  appId: "1:1089940570781:web:acb62a1b8fc8078611ca74"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
