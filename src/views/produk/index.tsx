68909a53b6bb179c357b9c03ef516140710431c3
azzazhr <azzahraqina70@gmail.com>
2026-04-06 20:35:15 +0700

    add Jobsheet 8 (Bagian 2 modifikasi file index.tsx pada /views/produk)

--- src/views/produk/index.tsx
+++ src/views/produk/index.tsx
@@ -1,6 +1,4 @@
-import HeroSection from "./hero.section";
-import MainSection from "./main.section";
-import styles from "./produk.module.css";
+import styles from "../../pages/produk/product.module.scss";
 
 type ProductType = {
   id: string;
@@ -12,17 +10,29 @@ type ProductType = {
 
 const TampilanProduk = ({ products }: { products: ProductType[] }) => {
   return (
-    <div>
-      <h1>Daftar Produk</h1>
+    <div className={styles.produk}>
+      <h1 className={styles.produk__title}>Daftar Produk</h1>
+      <div className={styles.produk__content}>
+        {products.map((products: ProductType) => (
+          <div key={products.id} className={styles.produk__content__item}>
+            <div className={styles.produk__content__item__image}>
+              <img src={products.image} alt={products.name} width={200} />
+            </div>
 
-      {products.map((products: ProductType) => (
-        <div key={products.id}>
-          <h2>Nama: {products.name}</h2>
-          <p>Harga: {products.price}</p>
-          <img src={products.image} alt={products.name} width={200} />
-          <p>Kategori: {products.category}</p>
-        </div>
-      ))}
+            <h4 className={styles.produk__content__item__name}>
+              {products.name}
+            </h4>
+
+            <p className={styles.produk__content__item__category}>
+              {products.category}
+            </p>
+
+            <p className={styles.produk__content__item__price}>
+              Rp {products.price.toLocaleString()}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
