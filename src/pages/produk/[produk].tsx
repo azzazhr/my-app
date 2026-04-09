143b82c4abb3af2922070bf773c032721ec9bf45
azzazhr <azzahraqina70@gmail.com>
2026-04-09 15:54:49 +0700

    add Jobsheet 11 (Bagian 3 - modifikasi file [produk].tsx untuk implementasi SSR halaman detail produk)

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -2,24 +2,42 @@ import fetcher from "@/utils/swr/fetcher";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import DetailProduk from "../../views/DetailProduct";
+import { ProductType } from "@/types/Product.type";
 
-const HalamanProduk = () => {
-  const { query } = useRouter();
-
-  const { data, error, isLoading } = useSWR(
-    query.produk ? `/api/produk/${query.produk}` : null,
-    fetcher
-  );
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Terjadi error</div>;
-  if (!data?.data) return <div>Data tidak ditemukan</div>;
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+  //digunakan client-side rendering*
+  // const Router = useRouter();
+  // console.log(Router);
+  // const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/products/${query.produk}`,
+  //   fetcher,
+  // );
+  // return (
+  //   <div>
+  //     <DetailProduk products={isLoading ? [] : data.data} />
+  //   </div>
+  // );
 
   return (
     <div>
-      <DetailProduk products={data.data} />
+      <DetailProduk products={product} />
     </div>
   );
 };
 
 export default HalamanProduk;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+//digunakan server-side rendering
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const response = await res.json();
+  // console.log("Data produk yang diambil dari API:", response);
+
+  return {
+    props: {
+      product: response.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
