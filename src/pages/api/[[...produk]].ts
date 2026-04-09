1a80cf798fb2e4f1329bdd234b667ab8d14457ff
azzazhr <azzahraqina70@gmail.com>
2026-04-09 14:10:53 +0700

    add Jobsheet 11 (Bagian 2 - modifikasi file [[...produk]].ts untuk implementasi CSR)

--- src/pages/api/[[...produk]].ts
+++ src/pages/api/[[...produk]].ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { retrieveProducts } from "../../utils/db/servicefirebase";
-
+import {
+  retrieveDataByID,
+  retrieveProducts,
+} from "../../utils/db/servicefirebase";
 
 type Data = {
   status: boolean;
@@ -11,8 +13,17 @@ type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  const data = await retrieveProducts("products");
-  res.status(200).json({ status: true, status_code: 200, data });
+  if (req.query.produk![1]) {
+    const data = await retrieveDataByID(
+      "products",
+      req.query.produk![1] as string
+    );
+    res.status(200).json({ status: true, status_code: 200, data });
+    return;
+  } else {
+    const data = await retrieveProducts("products");
+    res.status(200).json({ status: true, status_code: 200, data });
+  }
 }
