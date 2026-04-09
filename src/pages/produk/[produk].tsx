81ec9c694a3a22e4f8a1079055d3e2e8958acec3
azzazhr <azzahraqina70@gmail.com>
2026-04-09 15:00:25 +0700

    add Jobsheet 11 (Bagian 2 - modiifkasi file [produk].tsx untuk implementasi CSR

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -1,6 +1,7 @@
 import fetcher from "@/utils/swr/fetcher";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import DetailProduk from "../../views/DetailProduct";
 
 const HalamanProduk = () => {
   // const Router = useRouter();
@@ -8,14 +9,13 @@ const HalamanProduk = () => {
 
   const { query } = useRouter();
   const { data, error, isLoading } = useSWR(
-    `/api/products/${query.produk}`,
+    `/api/produk/${query.produk}`,
     fetcher
   );
 
   return (
     <div>
-      <h1>Halaman Produk</h1>
-      <p>Produk: {query.produk}</p>
+      <DetailProduk products={isLoading ? [] : data.data} />
     </div>
   );
 };
