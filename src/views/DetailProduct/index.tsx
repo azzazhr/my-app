62157548a7a2ea80906338e50ad4021f58cfb15e
azzazhr <azzahraqina70@gmail.com>
2026-04-09 15:10:03 +0700

    add Jobsheet 11 (Bagian 2 - modifikasi untuk mempercantik tulisan detail produk)

--- src/views/DetailProduct/index.tsx
+++ src/views/DetailProduct/index.tsx
@@ -6,7 +6,7 @@ const DetailProduk = ({ products }: { products: ProductType }) => {
 
   return (
     <>
-      <h1>Detail Produk</h1>
+      <h1 className={styles.title}>Detail Produk</h1>
       <div className={styles.produkdetail}>
         <div className={styles.produkdetail__image}>
           <img src={products.image} alt={products.name} />
