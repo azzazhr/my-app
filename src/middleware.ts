b8b4cfd0d01875a2058edb4bc01c407929832490
azzazhr <azzahraqina70@gmail.com>
2026-04-10 15:19:40 +0700

    add Jobsheet 14 (Bagian Proteksi Hal Profile - modifikasi kode benar pada file middleware.ts untuk membuat middleware authorization)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,13 @@
-import { getToken } from "next-auth/jwt";
-import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import withAuth from "./middleware/withAuth";
 
-export default function withAuth(
-  middleware: NextMiddleware,
-  requireAuth: string[] = []
-) {
-  return async (req: NextRequest, next: NextFetchEvent) => {
-    const pathname = req.nextUrl.pathname;
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
+  },
+  ["/profile"]
+);
 
-    if (requireAuth.includes(pathname)) {
-      const token = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-      });
-
-      if (!token) {
-        const loginUrl = new URL("/", req.url);
-        return NextResponse.redirect(loginUrl);
-      }
-    }
-
-    return middleware(req, next);
-  };
-}
+export const config = {
+  matcher: ["/profile"],
+};
