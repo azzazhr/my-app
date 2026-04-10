f23d3373e2eef7f9ffdf201c59ae4b92e6a78504
azzazhr <azzahraqina70@gmail.com>
2026-04-10 13:10:23 +0700

    add Jobsheet 14 (Bagian 5 - modifikasi file index.tsx pada /component/navbar untuk menambahkan tombol login dan logout)

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,10 +1,13 @@
 import styles from './navbar.module.css';
+import { signIn, signOut, useSession } from "next-auth/react"
+
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <div className="big">
                 <h1>Navbar</h1>
             </div>
+                <button onClick={() => signIn()}>Sign In</button>
         </div>
     );
 };
