fca9c4dfe80fa60a094515cd56ba77c9096b6831
azzazhr <azzahraqina70@gmail.com>
2026-04-10 13:55:10 +0700

    add Jobsheet 14 (Bagian Proteksi Hal Profile - modifikasi file index.tsx pada /pages/profile untuk membuat hal profile)

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,14 @@
-const Profile = () => {
+import { useSession } from "next-auth/react";
+
+const HalamanProfile = () => {
+  const { data }: any = useSession();
+
   return (
     <div>
-      <h1>Halaman Profile</h1>
-      <p>Ini halaman profile</p>
+      <h1>Halaman Profile</h1><br />
+      <h1>Selamat Datang {data?.user?.fullname}</h1>
     </div>
   );
 };
 
-export default Profile;
+export default HalamanProfile;
