108ecaefad86186993db67e8b30ee302137090fa
azzazhr <azzahraqina70@gmail.com>
2026-04-10 09:29:55 +0700

    add Jobsheet 13 (Bagian 1 - modifikasi file index.tsx di /pages/produk untuk membuat middleware)

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -1,22 +1,20 @@
-import TampilanProduk from "@/views/produk";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../../views/produk";
 import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
 // const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const kategori = () => {
-  // const [isLogin, setIsLogin] = useState(false);
+  // const [isLogin, setIsLogin] = useState(true);
+  const { push } = useRouter();
   const [products, setProducts] = useState([]);
 
   // console.log("products:", products);
 
   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
-
   // cek apakah data, error, dan isLoading sudah benar
-  // console.log("data:", data);
-  // console.log("error:", error);
-  // console.log("isLoading:", isLoading);
 
   return (
     <div>
