aa703a411650e6f2258122f275f7ef920362d365
azzazhr <azzahraqina70@gmail.com>
2026-04-17 10:09:28 +0700

    add Jobsheet 15 (Bagian 1 - modifikasi file index.tsx pada /views/auth/register/ untuk membuat register view)

--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -1,40 +1,11 @@
 import Link from "next/link";
-// import styles from "./register.module.css";
+import style from "../../../auth/register/register.module.scss";
 
 const TampilanRegister = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-green-400">
-      
-      <div className="bg-white p-8 rounded-lg shadow-md flex flex-col items-center gap-3">
-        <h1 className="text-2xl font-bold">Halaman Register</h1>
-
-        <input
-          type="text"
-          placeholder="Username"
-          className="border px-3 py-2 rounded w-64"
-        />
-
-        <input
-          type="email"
-          placeholder="Email"
-          className="border px-3 py-2 rounded w-64"
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          className="border px-3 py-2 rounded w-64"
-        />
-
-        <button className="border px-3 py-1 rounded">
-          Register
-        </button>
-
-        <Link href="/auth/login" className="text-blue-600">
-          Ke Halaman Login
-        </Link>
-      </div>
-
+    <div className={style.register}>
+      <h1 className={style.register__title}>Halaman Register</h1>
+      <Link href="/auth/login">Ke Halaman Login</Link>
     </div>
   );
 };
