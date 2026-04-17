6fbdf76acdd1cc9e1e35c4414e39d6e375e89ed5
azzazhr <azzahraqina70@gmail.com>
2026-04-17 10:16:48 +0700

    add Jobsheet 15 (Bagian 1 - modifikasi file index.tsx pada /views/auth/register/ untuk menambahkan form inputan)

--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -5,6 +5,27 @@ const TampilanRegister = () => {
   return (
     <div className={style.register}>
       <h1 className={style.register__title}>Halaman Register</h1>
+
+      <form className={style.register__form}>
+        
+        <div className={style.register__form__item}>
+          <label
+            htmlFor="email"
+            className={style.register__form__item__label}
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            className={style.register__form__item__input}
+          />
+        </div>
+
+      </form>
+
       <Link href="/auth/login">Ke Halaman Login</Link>
     </div>
   );
