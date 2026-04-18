a067e3e3d8c58e7e830c40ac8b27125c263b4fc5
azzazhr <azzahraqina70@gmail.com>
2026-04-18 23:08:15 +0700

    add Tugas No 2 Jobsheet 17 (modifikasi file index.tsx pada /pages/editor untuk membuat halaman khusus edtior)

---
+++ src/pages/editor/index.tsx
@@ -0,0 +1,17 @@
+const HalamanEditor = () => {
+  return (
+    <div>
+      <div className="editor">
+        <h1>Halaman Editor</h1>
+        <p>
+          Selamat datang di halaman editor. Halaman ini khusus untuk pengguna
+          dengan role editor. Di sini editor dapat mengelola konten, melakukan
+          perubahan data tertentu, dan menjalankan tugas yang diberikan tanpa
+          memiliki akses penuh seperti admin.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanEditor;
