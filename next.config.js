5fb391ccec57cd47e7f658e26f8d8e9e8564401d
azzazhr <azzahraqina70@gmail.com>
2026-04-23 19:25:49 +0700

    add Jobsheet 18 (Prak1 - image optimization bagian b optimasi gambar remote dengan modifikasi file next.config.js)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com", "encrypted-tbn0.gstatic.com",],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.adidas.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
