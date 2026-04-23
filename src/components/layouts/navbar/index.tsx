fd53890ee4d21017ecd518ba42ba1ea440d21f63
azzazhr <azzahraqina70@gmail.com>
2026-04-23 19:40:32 +0700

    add Jobsheet 18 (Prak3 - script optimization dengan modifikasi file index pada /layouts/navbar)

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./navbar.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import Script from "next/dist/client/script";
 
 const Navbar = () => {
   const { data }: any = useSession();
@@ -9,9 +10,11 @@ const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
-      <div className={styles.navbar__brand}>
-        MyApp
-      </div>
+      <div className={styles.navbar__brand} id="title"></div>
+
+      <Script id="title-script" strategy="lazyOnload">
+        {`document.getElementById('title').innerHTML = 'MyApp';`}
+      </Script>
 
       <div className={styles.navbar__right}>
         {data ? (
