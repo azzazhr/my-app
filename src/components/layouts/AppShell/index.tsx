89cb18260d18001ed1891f624c046e0bc73b9b5e
azzazhr <azzahraqina70@gmail.com>
2026-04-23 19:35:01 +0700

    add Jobsheet 18 (Prak2 - font optimization dengan modifikasi file index pada AppShell)

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Navbar from "../navbar";
 import Footer from "../footer";
+import { Roboto } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register", '/404'];
 
@@ -8,13 +9,18 @@ type AppShellProps = {
   children: React.ReactNode;
 }
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
   const { pathname } = useRouter();
   return (
     <div className="app-container">
 
-      <main className="main-content">
+      <main className={roboto.className}>
         {!disableNavbar.includes(pathname) && <Navbar /> }
         {children}
       </main>
