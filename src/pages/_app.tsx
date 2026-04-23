6068541adcfd06d6c27d24393095eac4bc38f567
azzazhr <azzahraqina70@gmail.com>
2026-04-23 20:32:25 +0700

    add Tugas No 3 Jobsheet 18 (menambahkan script google analytics menggunakan next/script)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,33 @@ import type { AppProps } from "next/app";
 import AppShell from "@/components/layouts/AppShell";
 import Navbar from "@/components/layouts/navbar";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
-    </SessionProvider>
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XXXXXXXXXX');
+        `}
+      </Script>
+
+      <SessionProvider session={pageProps.session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+    </>
   );
 }
