cf572ff8bc558812002c4ae2e606bda0d1f2d1c7
azzazhr <azzahraqina70@gmail.com>
2026-04-24 20:53:19 +0700

    add Jobsheet 19 - Prak8 (modifikasi file untuk menangani undefined data)

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -18,7 +18,7 @@ const kategori = () => {
 
   return (
     <div>
-      <TampilanProduk products={isLoading ? [] : data.data} />
+      <TampilanProduk products={isLoading ? [] : data?.data} />
     </div>
   );
 };
