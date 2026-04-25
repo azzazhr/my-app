19a8ac7e2b436c3e0d7832b9b435a43d45b15196
azzazhr <azzahraqina70@gmail.com>
2026-04-25 20:11:35 +0700

    restore middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,12 @@
 import { NextResponse } from "next/server";
-// import withAuth from "./middleware/withAuth";
+import withAuth from "./middleware/withAuth";
 
-// export default withAuth(
-//   function middleware() {
-//     return NextResponse.next();
-//   },
-//   ["/profile", "/admin", "/editor"]
-// );
-
-export function middleware() {
-  return NextResponse.next();
-}
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
+  },
+  ["/profile", "/admin", "/editor"]
+);
 
 export const config = {
   matcher: ["/profile", "/admin", "/editor"], 
